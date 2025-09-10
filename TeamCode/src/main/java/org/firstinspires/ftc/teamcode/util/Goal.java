646eaec42c4ff863adae8f1632354cee4e34eff6
June <68359573+CT1138@users.noreply.github.com>
2025-09-09 19:27:18 -0700

    nearly lost my mind but i think it works (also turns out you cant check servo pos, only target, fml)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Goal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Goal.java
@@ -2,15 +2,41 @@ package org.firstinspires.ftc.teamcode.util;
 
 import static java.lang.Thread.sleep;
 
-import androidx.annotation.NonNull;
-
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Goal {
     private Servo[] Servos;
     private DcMotor[] Motors;
     private int[] Positions;
+    private final ReentrantLock lock = new ReentrantLock();
+    private final Condition motorsNotBusy = lock.newCondition();
+
+    private boolean hardwareIsBusy() {
+        for (DcMotor motor : Motors) {
+            if (motor.getCurrentPosition() != motor.getTargetPosition() || motor.isBusy()) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private void waitForHardwareToStop() {
+        lock.lock();
+        try {
+            while (hardwareIsBusy()) {
+                try {
+                    motorsNotBusy.await();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    return;
+                }
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
 
     public Goal(DcMotor[] m_motors, Servo[] s_servo, int[] iPositions) {
         this.Motors = m_motors;
@@ -18,7 +44,7 @@ public class Goal {
         this.Servos = s_servo;
     }
 
-    public void RunToGoal(double power, double waitAfter) {
+    public void RunToGoal(double power, double waitAfter) throws InterruptedException {
         for (int i = 0; i < Motors.length; i++) {
             Motors[i].setPower(power);
             Motors[i].setTargetPosition(Positions[i]);
@@ -27,5 +53,7 @@ public class Goal {
         for (int i = 0; i < Servos.length; i++) {
             Servos[i].setPosition(Positions[Motors.length + i]);
         }
+        waitForHardwareToStop();
+        wait((long) waitAfter);
     }
 }
